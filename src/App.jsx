b4257b8ebf5cd81215a70c3de2ca52b6b4694efe
kwak-jae-yong <dream001211@naver.com>
2025-03-27 11:02:46 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import Header from './components/Header'
+import TodoEditor from './components/TodoEditor'
+import TodoList from './components/TodoList'
+import './App.css'
+import {useState,useRef} from 'react'
+
+const mockDate=[
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime()
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '산책하기',
+    date: new Date().getTime()
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '노래연습하기',
+    date: new Date().getTime()
+  },
+]
+
+function App() {
+  const [todos,setTodos]=useState(mockDate)
+  const idRef = useRef(3)
+
+  const onCreate = (content)=>{
+    const newTodo={
+      id:idRef.current++,
+      isDone:false,
+      content:content,
+      date:new Date().getTime()
+    }
+    setTodos([newTodo,...todos])
+  }
+
+  const onUpdate = (targetId)=>{
+    setTodos(
+      todos.map((todo)=>
+        todo.id===targetId
+      ?{...todo, isDone: !todo.isDone}:todo
+      )
+    )
+  }
+  const onDelete=(targetId)=>{
+    setTodos(
+      todos.filter((todo)=>todo.id!==targetId)
+    )
+  }
+  return (
+    <div className='App'>
+       <Header />
+      <TodoEditor onCreate={onCreate}/>
+      <TodoList todos={todos}onUpdate={onUpdate} onDelete={onDelete}/>
+    </div>
+  )
+}
+
+export default App
